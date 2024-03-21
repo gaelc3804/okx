bb055f30da74e2d1e6120c58a7bb1f035741acd5
Your Name <you@example.com>
2024-03-21 11:22:17 -0300

    redirecting by lang

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import { getIpGeo } from "@/utils";
 import { redirect } from "next/navigation";
 
 export default async function PageRed() {
+  const req = fetch("https://api.ipify.org?format=json");
+  const data = await (await req).json();
+
+  const { ip } = data;
+
+  // console.log(ip);
+  const locData = await getIpGeo(ip);
+  if (locData === "CN") redirect("/cn/login");
   redirect("/en/login");
 
   return <></>;
